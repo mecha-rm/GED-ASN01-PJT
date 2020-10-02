e68f4282ba86ecb526022db326e5f72d65d2d2b0
Roderick Montague <roderick.montague243@gmail.com>
2020-10-01 22:43:19 -0400

    Working on Adding Prefabs
    I'm working on adding prefabs to the game.  I added a monkey material just for testing purposes. I'm planning on having a button that can spawn a certain prefab, but I don't know if that's viable for a good UI.

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+/*
+ * References:
+    - https://docs.unity3d.com/ScriptReference/Transform-position.html
+    - https://docs.unity3d.com/ScriptReference/Transform-rotation.html
+    - https://docs.unity3d.com/ScriptReference/Transform-localScale.html
+ */
+// level editor namespace
+namespace GED
+{
+    public class Object : MonoBehaviour
+    {
+
+        public string name = "";
+        public string description = "";
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
